5b061f5ff1cbc7c361fa7f1e471788218c25d6a4
jarvisNim <asiason21@gmail>
2023-10-29 00:37:00 +0900

    add batch job for s_korea.py

--- batch/minerenv.sh
+++ batch/minerenv.sh
@@ -4,4 +4,5 @@ source ~/anaconda3/bin/activate minerenv #correct
 #python Documents/my_python_file_name.py WRONG SEPARATLY GO TO FOLER WHTAN EXECUTE EITH python
 cd /home/jarvis/codes/Minerva/ #correct
 python ./batch/sentiment.py > /home/jarvis/logs/cronError.log 2>&1  #correct
+python ./batch/s_korea.py > /home/jarvis/logs/cronError.log 2>&1
 conda deactivate
