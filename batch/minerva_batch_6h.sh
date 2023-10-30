a0fc317445ac6a6174516751ecba0a10c32b5180
jarvisNim <asiason21@gmail>
2023-10-30 14:10:36 +0900

    add china.py

--- batch/minerva_batch_6h.sh
+++ batch/minerva_batch_6h.sh
@@ -5,4 +5,5 @@ source ~/anaconda3/bin/activate minerenv #correct
 cd /home/jarvis/codes/Minerva/ #correct
 python ./batch/sentiment.py > /home/jarvis/logs/cronError.log 2>&1  #correct
 python ./batch/s_korea.py > /home/jarvis/logs/cronError.log 2>&1
+python ./batch/china.py > /home/jarvis/logs/cronError.log 2>&1
 conda deactivate
