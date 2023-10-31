c6138fb930921571a51df948ba2aeb454c078bd7
jarvisNim <asiason21@gmail>
2023-10-31 10:27:53 +0900

    add2 japan.py

--- batch/minerva_batch_6h.sh
+++ batch/minerva_batch_6h.sh
@@ -6,4 +6,5 @@ cd /home/jarvis/codes/Minerva/ #correct
 python ./batch/sentiment.py > /home/jarvis/logs/cronError.log 2>&1  #correct
 python ./batch/s_korea.py > /home/jarvis/logs/cronError.log 2>&1
 python ./batch/china.py > /home/jarvis/logs/cronError.log 2>&1
+python ./batch/japan.py > /home/jarvis/logs/cronError.log 2>&1
 conda deactivate
