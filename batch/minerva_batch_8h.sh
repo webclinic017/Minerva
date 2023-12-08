696d9188c5f97e76e123d7b63e63e260854b1ab4
jarvisNim <asiason21@gmail>
2023-12-08 09:25:08 +0900

    logging 2 full text with pandas.set_option

--- batch/minerva_batch_8h.sh
+++ batch/minerva_batch_8h.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-env >> /home/jarvis/logs/cronError.log 2>&1
 source ~/anaconda3/bin/activate minerenv #correct
-env >> /home/jarvis/logs/cronError.log 2>&1
 cd /home/jarvis/codes/Minerva/ #correct
-cp ./database/Economics.db ./database/Economics.db.backup
-python ./batch/economics_db.py >> /home/jarvis/logs/cronError.log 2>&1
 
-python ./batch/sentiment.py >> /home/jarvis/logs/cronError.log 2>&1  #correct
-python ./batch/kr_ecos.py >> /home/jarvis/logs/cronError.log 2>&1
-python ./batch/kr_marks.py >> /home/jarvis/logs/cronError.log 2>&1
-python ./batch/jp_ecos.py >> /home/jarvis/logs/cronError.log 2>&1
-python ./batch/in_ecos.py >> /home/jarvis/logs/cronError.log 2>&1
-python ./batch/global.py >> /home/jarvis/logs/cronError.log 2>&1
-python ./batch/cn_ecos.py >> /home/jarvis/logs/cronError.log 2>&1
-python ./batch/de_ecos.py >> /home/jarvis/logs/cronError.log 2>&1
-python ./batch/us_ecos.py >> /home/jarvis/logs/cronError.log 2>&1
-python ./batch/us_marks.py >> /home/jarvis/logs/cronError.log 2>&1
+cp ./database/Economics.db ./database/Economics.db.backup
+python ./batch/economics_db.py
+
+python ./batch/sentiment.py
+python ./batch/kr_ecos.py
+python ./batch/kr_marks.py
+python ./batch/jp_ecos.py
+python ./batch/in_ecos.py
+python ./batch/global.py
+python ./batch/cn_ecos.py
+python ./batch/de_ecos.py
+python ./batch/us_ecos.py
+python ./batch/us_marks.py
 
 conda deactivate
