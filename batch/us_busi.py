c8590f1172d6dfa19d733b0f327b458ed6e9f870
jarvisNim <asiason21@gmail>
2023-12-29 16:44:51 +0900

    fixing bugs

--- batch/us_busi.py
+++ batch/us_busi.py
@@ -49,17 +49,8 @@ def make_plot(ticker, df, trend, tech_type, sbubplot_cnt, idx):
     
     _df = df.copy() # 그래프만 그리면 되고, 본 df 에는 영향을 주지않기위해 복사본 만듬.
     
-    if slope == 'STAY':  # 상하위 값이 없는 경우, 즉 threshold 를 crossover 만 탐색하는 경우 사용하는 루틴: trend 불필요 
-        h_limit = 0
-        l_limit = 0
-        try:
-            _df['pivot'] = np.where((_df[tech_type].shift(1)*_df[tech_type]) < 0, 1, 0)  # 1로 되는 일자부터 매수 또는 매도후 현금
-            __ = _df[_df['pivot'] == 1]
-            latest_date = __.index[-1]
-        except:
-            latest_date = '9999-99-99'
     # 상하위 값의 밴드를 가지고 있는 경우, 상하위 값을 벗어나면 탐색, trend 필요, trend 에 따라 밴드폭 조절
-    elif slope == 'UP':  # 확장 국면
+    if slope == 'UP':  # 확장 국면
         # 85% 분포를 갖는 시그마: 1.0364333894937898
         h_limit = _df[tech_type].mean() + (SIGMA_85 * _df[tech_type].std())
         l_limit = _df[tech_type].mean() - (SIGMA_85 * _df[tech_type].std())
@@ -71,13 +62,35 @@ def make_plot(ticker, df, trend, tech_type, sbubplot_cnt, idx):
             latest_date = max(_1.index[-1], _2.index[-1])
         except:
             latest_date = '9999-99-99'
-    else:  # 수축 국면
+
+    elif slope == 'DOWN':  # 수축 국면
         # 75% 분포를 갖는 시그마: 0.6744897501960817
         h_limit = _df[tech_type].mean() + (SIGMA_75 * _df[tech_type].std())
         l_limit = _df[tech_type].mean() - (SIGMA_75 * _df[tech_type].std())
         h_norm = _df[tech_type].mean() + _df[tech_type].std()
-        l_norm = _df[tech_type].mean() - _df[tech_type].std()
+        l_norm = _df[tech_type].mean() - _df[tech_type].std()   
+        try:
+            _df['pivot'] = np.where((_df[tech_type] > h_limit) & (_df[tech_type].shift(1) <= h_limit), 1, 0)
+            _df['pivot'] = np.where((_df[tech_type] < l_limit) & (_df[tech_type].shift() >= l_limit), -1, _df['pivot'])         
+            _1 = _df[_df['pivot'] == 1]
+            _2 = _df[_df['pivot'] == -1]
+            latest_date = max(_1.index[-1], _2.index[-1])
+        except:
+            latest_date = '9999-99-99'                 
 
+    elif slope == 'NA':  # 상하위 값이 없는 경우, 즉 threshold 를 crossover 만 탐색하는 경우 사용하는 루틴: trend 불필요 
+        h_limit = 0
+        l_limit = 0
+        try:
+            _df['pivot'] = np.where((_df[tech_type].shift(1)*_df[tech_type]) < 0, 1, 0)  # 1로 되는 일자부터 매수 또는 매도후 현금
+            __ = _df[_df['pivot'] == 1]
+            latest_date = __.index[-1]
+        except:
+            latest_date = '9999-99-99'
+
+    else:  # 'STAY' 유지 국면
+        h_norm = _df[tech_type].mean() + _df[tech_type].std()
+        l_norm = _df[tech_type].mean() - _df[tech_type].std()
         try:
             _df['pivot'] = np.where((_df[tech_type] > h_limit) & (_df[tech_type].shift(1) <= h_limit), 1, 0)
             _df['pivot'] = np.where((_df[tech_type] < l_limit) & (_df[tech_type].shift() >= l_limit), -1, _df['pivot'])         
@@ -139,7 +152,7 @@ def bb(ticker, df, slope, tech_type, sbubplot_cnt, idx):
 def sma(ticker, df, slope, tech_type, sbubplot_cnt, idx):
     
     df[tech_type] = df.ta.sma(20) - df.ta.sma(200)
-    slope = 'STAY'
+    slope = 'NA'
     latest_date = make_plot(ticker, df, slope, tech_type, sbubplot_cnt, idx)
     
     return df, latest_date
@@ -185,58 +198,63 @@ if __name__ == "__main__":
     _trend = CalTrend()
 
     
-    for ticker in WATCH_TICKERS:
+    for x in WATCH_TICKERS['US']:
 
-        nation = 'US'
-        asset = 'stock'
-        ticker = ticker
-        trend_now = _trend.cal_trend(nation, asset, ticker, 0)
-        trend_6mo = _trend.cal_trend(nation, asset, ticker, 6)
-        diff = trend_now - trend_6mo
-        if diff > 0:  # 수축 국면
-            slope = 'DOWN'
-        elif  diff == 0:  # 유지 국면
-            slope = 'STAY'
-        else:
-            slope = 'UP'
-        logger2.info('')
-        logger2.info(f'##### {nation} Trend Now: {trend_now} %')
-        logger2.info(f'##### {nation} Trend after 6 month: {trend_6mo} %')
-        logger2.info(f'##### {nation} Economic Trend: {slope}')        
+        for key, item in x.items():
+
+            for ticker in item:
+                nation = 'US'
+                asset = key
+                if ticker == '':
+                    continue                
+
+                trend_now = _trend.cal_trend(nation, asset, ticker, 0)
+                trend_6mo = _trend.cal_trend(nation, asset, ticker, 6)
+                diff = trend_now - trend_6mo
+                if diff > 0:  # 수축 국면
+                    slope = 'DOWN'
+                elif  diff == 0:  # 유지 국면
+                    slope = 'STAY'
+                else:
+                    slope = 'UP'
+                logger2.info('')
+                logger2.info(f'##### {nation} Trend Now: {trend_now} %')
+                logger2.info(f'##### {nation} Trend after 6 month: {trend_6mo} %')
+                logger2.info(f'##### {nation} Economic Trend: {slope}')        
             
           
-        sbubplot_cnt = len(ta_list)
-        plt.figure(figsize=(18, 4*sbubplot_cnt))          
-        df = get_data(ticker, 20)
-        # Volume.Money Flow Index (MFI)
-        tech_type = 'mfi'
-        df, latest_date = mfi(ticker, df, slope, tech_type, sbubplot_cnt, 1)
-        print(latest_date)
+                sbubplot_cnt = len(ta_list)
+                plt.figure(figsize=(18, 4*sbubplot_cnt))          
+                df = get_data(ticker, 20)
+                # Volume.Money Flow Index (MFI)
+                tech_type = 'mfi'
+                df, latest_date = mfi(ticker, df, slope, tech_type, sbubplot_cnt, 1)
+                print(latest_date)
 
-        # Momentum.Percentage Price Oscillator (PPO)
-        tech_type = 'ppo'
-        df, latest_date = ppo(ticker, df, slope, tech_type, sbubplot_cnt, 2)
-        print(latest_date)
+                # Momentum.Percentage Price Oscillator (PPO)
+                tech_type = 'ppo'
+                df, latest_date = ppo(ticker, df, slope, tech_type, sbubplot_cnt, 2)
+                print(latest_date)
 
-        # Volatility.Bollinger Bands (BB)
-        tech_type = 'bb'
-        df, latest_date = bb(ticker, df, slope, tech_type, sbubplot_cnt, 3)
-        print(latest_date)
+                # Volatility.Bollinger Bands (BB)
+                tech_type = 'bb'
+                df, latest_date = bb(ticker, df, slope, tech_type, sbubplot_cnt, 3)
+                print(latest_date)
 
-        # Trend.Simple Moving Average (SMA)
-        tech_type = 'sma'
-        df, latest_date = sma(ticker, df, slope, tech_type, sbubplot_cnt, 4)      
-        print(latest_date)
+                # Trend.Simple Moving Average (SMA)
+                tech_type = 'sma'
+                df, latest_date = sma(ticker, df, slope, tech_type, sbubplot_cnt, 4)      
+                print(latest_date)
 
-        # Momentum.Relative Strength Index (RSI)
-        tech_type = 'rsi'
-        df, latest_date = rsi(ticker, df, slope, tech_type, sbubplot_cnt, 5)      
-        print(latest_date)    
+                # Momentum.Relative Strength Index (RSI)
+                tech_type = 'rsi'
+                df, latest_date = rsi(ticker, df, slope, tech_type, sbubplot_cnt, 5)      
+                print(latest_date)    
 
-        # Volume.Volume Weighted Average Price (VWAP)
-        tech_type = 'vwap'
-        df, latest_date = vwap(ticker, df, slope, tech_type, sbubplot_cnt, 6)      
-        print(latest_date)
+                # Volume.Volume Weighted Average Price (VWAP)
+                tech_type = 'vwap'
+                df, latest_date = vwap(ticker, df, slope, tech_type, sbubplot_cnt, 6)      
+                print(latest_date)
 
 
 
