dac5884d84dc7838c6ccc4a5c381d334ddc8affd
jarvisNim <asiason21@gmail>
2024-01-16 07:53:21 +0900

    make IMF table 5

--- batch/economics_db.py
+++ batch/economics_db.py
@@ -400,14 +400,19 @@ def make_imf_outlook():
 
     url = 'https://www.imf.org/-/media/Files/Publications/WEO/WEO-Database/2023/WEOOct2023all.ashx'
 
-    if webbrowser.open(url):
-      
-        # 현재 운영체제 확인
+    # if webbrowser.open(url):
+    #     sleep(10)  # 다운로드 파일 사이즈가 10M 라서 시간이 필요     
 
-        sleep(10)  # 다운로드 파일 사이즈가 10M 라서 시간이 필요     
-   
-    current_os = platform.system()
-    print(current_os)
+    response = requests.get(url)
+    if response.status_code == 200:
+        current_os = platform.system()
+        print(current_os)
+    else:
+        print(f"다운로드 실패. 응답 코드: {response.status_code}")
+
+    # 현재 운영체제 확인  
+    # current_os = platform.system()
+    # print(current_os)
     # 운영체제에 따른 조건문
     if current_os == 'Darwin':  # mac OS
         # print("현재 운영체제는 Windows입니다.")
